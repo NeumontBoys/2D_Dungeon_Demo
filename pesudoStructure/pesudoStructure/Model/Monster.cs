2b291af4b45473049824de2c378c4f0a49117f32
Stephen Derenski <33239267+Sderenski@users.noreply.github.com>
2022-05-10 15:14:51 -0600

    Create Monster.cs

---
+++ pesudoStructure/pesudoStructure/Model/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pesudoStructure.Model
+{
+    class Monster
+    {
+        public enum Monsters
+        {
+            Zombie,
+            Skuleton,
+            Troll,
+            Goblin,
+            Spider,
+            FireZombie,
+        }
+
+        public enum FinalMonstors
+        {
+
+        }
+    }
+}
